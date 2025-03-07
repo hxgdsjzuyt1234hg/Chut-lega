8f615dddba7b97a53ffceca9a40b953a9850b4ce
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-03-07 09:01:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // BOT TELE By Toya
 module.exports = {
-  BOT_TOKEN: "7946724859:AAG4DeTdZvCSXpiPR48JSAMahRo-pOQZ43w", // Token bot Telegram
-  OWNER_ID: "6552202106", // ID pemilik bot
+  BOT_TOKEN: "8044604306:AAEuz52022yLlbAvoC7X4Vk2W7WY2hnFajk", // Token bot Telegram
+  OWNER_ID: "7886419837", // ID pemilik bot
   allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
 };
